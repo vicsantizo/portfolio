e6c1df59e3362969e07dd4bae485d415ba4869bc
vicsantizo <santizo.vic@gmail.com>
2023-07-13 23:22:30 -0600

    feat: create portfolio's logo component

---
+++ src/components/ui/logo/logo.tsx
@@ -0,0 +1,14 @@
+import './logo.css';
+
+export function Logo() {
+  return (
+    <div className="logo">
+      <a href="/" className="logo__title">
+        <h1>Victor Fernández</h1>
+      </a>
+      <span className="logo__text">Front-end Developer</span>
+    </div>
+  );
+}
+
+export default Logo;
