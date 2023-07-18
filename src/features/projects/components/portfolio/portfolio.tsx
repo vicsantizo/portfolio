9adf52649d393e3e0a444c688be9e95b4ef737e6
vicsantizo <santizo.vic@gmail.com>
2023-07-18 02:12:16 -0600

    chore: add link to view all projects

--- src/features/projects/components/portfolio/portfolio.tsx
+++ src/features/projects/components/portfolio/portfolio.tsx
@@ -1,4 +1,5 @@
 import { Project } from '../project';
+import Link from 'next/link';
 import './portfolio.css';
 
 interface Project {
@@ -21,27 +22,81 @@ async function getProjects() {
 }
 
 export async function Portfolio() {
-  const projects = await getProjects();
+  // const projects = await getProjects();
+  const projects = [
+    {
+      id: 'read',
+      title: 'Read',
+      description: 'Personal tool for easily logging the reading progress of the books I am currently reading',
+      websiteLink: 'www.google.com',
+      githubLink: 'www.google.com',
+      imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
+    },
+    {
+      id: 'cashew',
+      title: 'Cashew',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      websiteLink: 'www.google.com',
+      githubLink: 'www.google.com',
+      imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
+    },
+    {
+      id: 'dashboard',
+      title: 'Dashboard',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      websiteLink: 'www.google.com',
+      githubLink: 'www.google.com',
+      imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
+    },
+    {
+      id: 'bubble',
+      title: 'Bubble',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      websiteLink: 'www.google.com',
+      githubLink: 'www.google.com',
+      imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
+    },
+    {
+      id: 'project5',
+      title: 'Project 5',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      websiteLink: 'www.google.com',
+      githubLink: 'www.google.com',
+      imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
+    },
+  ];
 
   return (
-    <main className="portfolio">
-      <h1 className="portfolio__title">Personal Projects</h1>
+    <>
+      <main className="portfolio">
+        <h1 className="portfolio__title">Personal Projects</h1>
 
-      <ul className="portfolio__projects">
-        {projects.map((project: Project) => (
-          <li key={project.id}>
-            <Project
-              id={project.id}
-              title={project.title}
-              description={project.description}
-              imgLink={project.imgLink}
-              githubLink={project.githubLink}
-              websiteLink={project.websiteLink}
-            />
-          </li>
-        ))}
-      </ul>
-    </main>
+        <ul className="portfolio__projects">
+          {projects.map((project: Project) => (
+            <li key={project.id}>
+              <Project
+                id={project.id}
+                title={project.title}
+                description={project.description}
+                imgLink={project.imgLink}
+                githubLink={project.githubLink}
+                websiteLink={project.websiteLink}
+              />
+            </li>
+          ))}
+        </ul>
+      </main>
+
+      <div className="portfolio__more">
+        <Link href="/projects" className="portfolio__link">
+          View all
+        </Link>
+      </div>
+    </>
   );
 }
 
