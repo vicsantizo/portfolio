fefbf954adb2a69ccb5469e66ab192d5798d463b
vicsantizo <santizo.vic@gmail.com>
2023-07-18 02:01:54 -0600

    chore: add ids to mocked data

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,5 +1,6 @@
 export const portfolioProjectsMock = [
   {
+    id: 'read',
     title: 'Read',
     description: 'Personal tool for easily logging the reading progress of the books I am currently reading',
     websiteLink: 'www.google.com',
@@ -7,6 +8,7 @@ export const portfolioProjectsMock = [
     imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
   },
   {
+    id: 'cashew',
     title: 'Cashew',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
@@ -15,6 +17,7 @@ export const portfolioProjectsMock = [
     imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
   },
   {
+    id: 'dashboard',
     title: 'Dashboard',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
@@ -23,6 +26,7 @@ export const portfolioProjectsMock = [
     imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
   },
   {
+    id: 'bubble',
     title: 'Bubble',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
@@ -31,6 +35,7 @@ export const portfolioProjectsMock = [
     imgLink: 'https://raw.githubusercontent.com/vicsantizo/read/main/read-logo.png',
   },
   {
+    id: 'project5',
     title: 'Project 5',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
