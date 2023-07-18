0a6a1bc9ab8195159dc765bcfe8199a34df6455b
vicsantizo <santizo.vic@gmail.com>
2023-07-18 00:33:34 -0600

    chore: modify project links

--- src/features/projects/components/project/project.tsx
+++ src/features/projects/components/project/project.tsx
@@ -3,40 +3,45 @@ import Image from 'next/image';
 import { shortenParagraph, shortenTitle } from '../../utils/text-format';
 import './project.css';
 
-type ProjectProps = {
+export type ProjectProps = {
+  id: string;
   title: string;
   description: string;
-  imgLink: string;
+  imgLink?: string;
   githubLink?: string;
   websiteLink?: string;
 };
 
 export function Project(props: ProjectProps) {
-  const { title, description, websiteLink, imgLink, githubLink } = props;
+  const { id, title, description, websiteLink, imgLink, githubLink } = props;
   const shortenedDescription = shortenParagraph(description, 100);
   const shortenedTitle = shortenTitle(title);
-  const isGithubLinkAvailable = Boolean(githubLink);
-  const isWebsiteLinkAvailable = Boolean(websiteLink);
 
   return (
     <div className="project" data-testid="project">
-      <div className="project__display">
-        <Image src={imgLink} width={60} height={60} alt={`${shortenedTitle} logo`} className="project__img" />
-      </div>
+      <Link href={`/projects/${id}`}>
+        <div className="project__display">
+          {imgLink && (
+            <Image src={imgLink} width={60} height={60} alt={`${shortenedTitle} logo`} className="project__img" />
+          )}
+        </div>
+      </Link>
 
       <div className="project__info">
-        <h2 className="project__title">{shortenedTitle}</h2>
+        <Link href={`/projects/${id}`} className="project__go">
+          <h2 className="project__title">{shortenedTitle}</h2>
+        </Link>
         <p className="project__desc">{shortenedDescription}</p>
       </div>
 
       <div className="project__links">
-        {isGithubLinkAvailable && (
+        {githubLink && (
           <Link href={githubLink!} target="_blank" className="project__link" title="Check GitHub repo">
             <Image src="/assets/icons/github.png" alt="" width={20} height={20} className="project__icon" />
           </Link>
         )}
-        {isWebsiteLinkAvailable && (
-          <Link href={websiteLink!} target="_blank" className="project__link" title="Go to website">
+        {websiteLink && (
+          <Link href={websiteLink} target="_blank" className="project__link" title="Go to website">
             <Image src="/assets/icons/website.png" alt="" width={20} height={20} className="project__icon" />
           </Link>
         )}
